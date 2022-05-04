27adae7713734c05c1c32ccccbe43a31a2f3286f
Viviane <viviane.sillva96@gmail.com>
2022-05-04 18:45:36 -0300

    Adicionar arquivos de projeto.

---
+++ CalcularIMC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculoIMC
+{
+     class CalcularIMC
+     {
+        public double Peso;
+        public double Altura;
+
+        public double CalcIMC()
+        {
+            return Peso /(Altura * Altura);
+        }
+     }
+}
